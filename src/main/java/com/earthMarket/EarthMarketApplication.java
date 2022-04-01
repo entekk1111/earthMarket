97dddee4f67c08962fd02ffd3bcd1e1454c670f6
entekk1111 <entekk1111@gmail.com>
2022-04-01 18:14:35 +0900

    first Commit

---
+++ src/main/java/com/earthMarket/EarthMarketApplication.java
@@ -0,0 +1,13 @@
+package com.earthMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earthMarket/EarthMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
