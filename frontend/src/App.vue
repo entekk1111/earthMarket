76aa84fc1c951f5d451f5f48640a2e167570c538
entekk1111 <entekk1111@gmail.com>
2022-05-03 23:07:39 +0900

    detail

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <List/>
+    <ProductDetail/>
   </div>
 </template>
 
 <script>
 import List from './views/List.vue';
+import ProductDetail from './views/ProductDetail.vue';
 
 export default {
   name: 'App',
   components: {
-    List
+    List,
+    ProductDetail
   }
 }
 </script>
