43f861ef35ea0be8d725b1ee3f7494dc91364b70
entekk1111 <entekk1111@gmail.com>
2022-05-18 19:15:44 +0900

    add proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "../src/main/resources/static",
+  devServer: {
+    port: 8080, // 접속 포트 변경
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+      },
+    }, // devServer :  Back-End , 즉 Spring Boot의 내장 was의 주소를 작성하게 되면 된다.
+  },
 });
