c3bba3d293099cf301d1d02eb35ecf14e2035f08
sunmin <dltjsals6356@gmail.com>
2022-06-10 09:13:36 +0900

    [스타일] 탭

---
+++ frontend/src/components/tab/TabMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tab-wrap">
+    <!-- tab-menu -->
+    <div class="tab-menu">
+      <div class="grab">
+        <button
+          class="menu"
+          v-for="(tab, index) in tabs"
+          :key="index"
+          :class="{ active: tab.isActive }"
+          @click="selectTab(tab)"
+        >
+          <span>{{ tab.name }} </span>
+        </button>
+        <a href="javascript:void(0);" class="btn-link"
+          ><IconiFy class="ico" icon="bi:flag-fill" />판매자신고</a
+        >
+      </div>
+    </div>
+    <!-- // tab-menu -->
+
+    <!-- tab-conten -->
+    <div class="tab-content grab">
+      <slot></slot>
+    </div>
+    <!-- // tab-content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabMenu",
+  data: function () {
+    return {
+      tabs: [],
+    };
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name === selectedTab.name;
+      });
+    },
+  },
+};
+</script>
