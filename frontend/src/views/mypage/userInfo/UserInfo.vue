5bf1667a0a6dde0c6e2e8710f51a3b41daab8bf1
sunmin <dltjsals6356@gmail.com>
2022-07-01 17:17:14 +0900

    [스타일] 마이페이지 회원정보 페이지 탭 추가

--- frontend/src/views/mypage/userInfo/UserInfo.vue
+++ frontend/src/views/mypage/userInfo/UserInfo.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="grab">회원정보</div>
+  <div>
+    <TabMenu>
+      <Tab name="회원정보" selected="true"> ㅇㅀㅇㅀㅇㅀ</Tab>
+      <Tab name="구매내역"> ssdfsdfsdf </Tab>
+      <Tab name="판매내역"> ssdfsdfsdf </Tab>
+      <Tab name="관심목록"> ssdfsdfsdf </Tab>
+      <Tab name="나의 리뷰"> ssdfsdfsdf </Tab>
+    </TabMenu>
+  </div>
 </template>
 
 <script>
+import Tab from "../../../components/tab/TabContent.vue";
+import TabMenu from "../../../components/tab/TabMenu.vue";
+
 export default {
   name: "UserInfo ",
+  components: {
+    Tab,
+    TabMenu,
+  },
 };
 </script>
+
+<style lang="scss">
+#container {
+  padding-right: 0;
+  padding-left: 0;
+}
+</style>
