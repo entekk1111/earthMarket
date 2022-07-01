396f6cd1cc8cede89e31ab837a16121b2a78d557
sunmin <dltjsals6356@gmail.com>
2022-07-01 14:04:08 +0900

    [스타일] 채팅페이지 퍼블리싱 작업중

--- frontend/src/views/chat/ChatView.vue
+++ frontend/src/views/chat/ChatView.vue
@@ -4,7 +4,11 @@
     <section class="section-left">
       <h2 class="tit-group">채팅</h2>
       <ul>
-        <li v-for="chatNavList in chatNavLists" :key="chatNavList">
+        <li
+          v-for="chatNavList in chatNavLists"
+          :key="chatNavList"
+          class="active"
+        >
           <a href="javascript:void(0);">
             <div class="profile-group">
               <div class="profile-img">
@@ -20,6 +24,36 @@
             </div>
           </a>
         </li>
+        <li>
+          <a href="javascript:void(0);"
+            ><div class="profile-group">
+              <div class="profile-img">
+                <img src="/img/sample01.86ecbf91.jpg" alt="" />
+              </div>
+              <strong>딸기노장</strong><span>킹사이즈 딸기 2kg...</span
+              ><em>13</em>
+            </div>
+            <div class="txt-group">
+              <p>직거래하고싶습니다! 연락가능한시간알려주세요</p>
+              <span>어제</span>
+            </div></a
+          >
+        </li>
+        <li class="disabled">
+          <a href="javascript:void(0);"
+            ><div class="profile-group">
+              <div class="profile-img">
+                <img src="/img/sample01.86ecbf91.jpg" alt="" />
+              </div>
+              <strong>딸기노장</strong><span>킹사이즈 딸기 2kg...</span
+              ><em>13</em>
+            </div>
+            <div class="txt-group">
+              <p>직거래하고싶습니다! 연락가능한시간알려주세요</p>
+              <span>어제</span>
+            </div></a
+          >
+        </li>
       </ul>
     </section>
     <!--  //section-left  -->
@@ -34,7 +68,7 @@
           <span>새콤달콤 설향딸기</span>
         </div>
         <div class="btn-group">
-          <a href="javascript:void(0);">상품보기</a>
+          <a href="javascript:void(0);" class="btn-line-black">상품보기</a>
           <a href="javascript:void(0);" class="btn btn-green">거래 완료</a>
         </div>
       </div>
@@ -256,23 +290,47 @@
           </div>
           <!--  // message-group -->
 
-          <div class="chat-result result-left">
-            <p>
-              판매자가 <strong>거래완료</strong>를 선택하였습니다. 구매를
-              완료하셨다면 버튼을 클릭해주세요.
-            </p>
-            <a href="javascript:void(0);">거래 완료</a>
-            <a href="javascript:void(0);">거래 완료 취소</a>
+          <!-- chat-result -->
+          <div class="chat-result mine">
+            <div class="inner">
+              <p>상품 판매 거래를 완료했습니다.</p>
+              <a href="javascript:void(0);" class="btn-line-gray"
+                >거래 완료 취소</a
+              >
+            </div>
           </div>
-          <div class="chat-result result-right">
-            <p>
-              판매자가 <strong>거래완료</strong>를 선택하였습니다. 구매를
-              완료하셨다면 버튼을 클릭해주세요.
-            </p>
-            <a href="javascript:void(0);">거래 완료 취소</a>
-            <a href="javascript:void(0);">리뷰 남기기</a>
+          <!--  // chat-result -->
+
+          <!-- chat-result -->
+          <div class="chat-result mine blue">
+            <div class="inner">
+              <p>
+                판매자가 <strong>거래완료</strong>를 선택하였습니다. 구매를
+                완료하셨다면 버튼을 클릭해주세요.
+              </p>
+              <a href="javascript:void(0);" class="btn btn-green-line"
+                >거래 완료</a
+              >
+            </div>
           </div>
+          <!--  // chat-result -->
+
+          <!-- chat-result  -->
+          <div class="chat-result other">
+            <div class="inner">
+              <p>
+                판매자가 <strong>거래완료</strong>를 선택하였습니다. 구매를
+                완료하셨다면 버튼을 클릭해주세요.
+              </p>
+              <a href="javascript:void(0);" class="btn btn-green-line"
+                >거래 완료 취소</a
+              >
+            </div>
+          </div>
+          <!-- //  chat-result  -->
         </div>
+
+        <!-- chat-input -->
         <div class="chat-input">
           <label for="chat"
             ><span class="sr-only">채팅글 작성 영역</span></label
@@ -280,6 +338,7 @@
           <input type="text" id="chat" placeholder="메시지를 입력해주세요" />
           <a href="javascript:void(0);">보내기</a>
         </div>
+        <!-- // chat-input -->
       </div>
       <!--  // chat-body -->
     </section>
