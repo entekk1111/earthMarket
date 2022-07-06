9d7298cfac8b7810aa780a9d3fb9b6c4b31ce40c
sunmin <dltjsals6356@gmail.com>
2022-07-06 16:34:49 +0900

    [스타일] 마이페이지_구매내역 퍼블리싱 작업중

---
+++ frontend/src/components/accordion/AccordionContent.vue
@@ -0,0 +1,69 @@
+<template>
+  <li>
+    <div :class="{ active: visible }" @click="open">
+      <slot name="accordion-trigger"></slot>
+    </div>
+    <transition
+      name="accordion"
+      @enter="start"
+      @after-enter="end"
+      @before-leave="start"
+      @after-leave="end"
+    >
+      <div v-show="visible">
+        <ul>
+          <slot name="accordion-content"></slot>
+        </ul>
+      </div>
+    </transition>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {},
+  inject: ["Accordion"],
+  data() {
+    return {
+      index: null,
+    };
+  },
+  computed: {
+    visible() {
+      return this.index == this.Accordion.active;
+    },
+  },
+  methods: {
+    open() {
+      if (this.visible) {
+        this.Accordion.active = null;
+      } else {
+        this.Accordion.active = this.index;
+      }
+    },
+    start(el) {
+      el.style.height = el.scrollHeight + "px";
+    },
+    end(el) {
+      el.style.height = "";
+    },
+  },
+  created() {
+    this.index = this.Accordion.count++;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.accordion-enter-active,
+.accordion-leave-active {
+  will-change: height, opacity;
+  transition: height 0.3s ease, opacity 0.3s ease;
+  overflow: hidden;
+}
+.accordion-enter,
+.accordion-leave-to {
+  height: 0 !important;
+  opacity: 0;
+}
+</style>
--- frontend/src/views/mypage/comp/BuyList.vue
+++ frontend/src/views/mypage/comp/BuyList.vue
@@ -1,9 +1,131 @@
 <template>
-  <div>구매내역</div>
+  <div class="buy-list">
+    <ul class="state">
+      <li>
+        <strong>거래중</strong>
+        <span>3</span>
+      </li>
+      <li>
+        <strong>구매완료</strong>
+        <span>12</span>
+      </li>
+    </ul>
+
+    <AccordionMenu>
+      <Accordion>
+        <template slot="accordion-trigger">
+          <div class="accordion-tit">
+            <h3 class="cont-tit02">거래중인 목록</h3>
+            <strong>3개상품</strong>
+            <IconiFy icon="mi:chevron-down" />
+          </div>
+        </template>
+
+        <template slot="accordion-content">
+          <ul>
+            <li v-for="item in lists" :key="item">
+              <div class="card-info">
+                <div class="profile-wrap">
+                  <div class="profile-img">
+                    <img :src="item.photo" alt="" />
+                  </div>
+                  <div class="txt">
+                    <span>{{ item.nickname }}</span>
+                    <p>{{ item.title }}</p>
+                    <strong>{{ item.money }}원</strong>
+                  </div>
+                </div>
+
+                <div class="btn-wrap">
+                  <a href="javascript:void(0);" class="btn btn-green"
+                    >거래완료</a
+                  >
+                  <b-dropdown class="btn-dropdown">
+                    <template #button-content>
+                      <IconiFy icon="mi:options-vertical" />
+                    </template>
+                    <b-dropdown-item>채팅방가기</b-dropdown-item>
+                    <b-dropdown-item>삭제하기</b-dropdown-item>
+                  </b-dropdown>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </template>
+      </Accordion>
+
+      <Accordion>
+        <template slot="accordion-trigger">
+          <div class="accordion-tit">
+            <h3 class="cont-tit02">구매완료 목록</h3>
+            <strong>3개상품</strong>
+            <IconiFy icon="mi:chevron-down" class="ico" />
+          </div>
+        </template>
+
+        <template slot="accordion-content">
+          <ul>
+            <li v-for="item in lists" :key="item">
+              <div class="card-info">
+                <div class="profile-wrap">
+                  <div class="profile-img">
+                    <img :src="item.photo" alt="" />
+                  </div>
+                  <div class="txt">
+                    <span>{{ item.nickname }}</span>
+                    <p>{{ item.title }}</p>
+                    <strong>{{ item.money }}원</strong>
+                  </div>
+                </div>
+
+                <div class="btn-wrap">
+                  <a href="javascript:void(0);" class="btn btn-green"
+                    >거래완료</a
+                  >
+                  <b-dropdown class="btn-dropdown">
+                    <template #button-content>
+                      <IconiFy icon="mi:options-vertical" class="ico" />
+                    </template>
+                    <b-dropdown-item>채팅방가기</b-dropdown-item>
+                    <b-dropdown-item>삭제하기</b-dropdown-item>
+                  </b-dropdown>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </template>
+      </Accordion>
+    </AccordionMenu>
+  </div>
 </template>
 
 <script>
+import Accordion from "../../../components/accordion/AccordionContent";
+import AccordionMenu from "../../../components/accordion/AccordionMenu";
+
 export default {
   name: "BuyList ",
+  components: {
+    AccordionMenu,
+    Accordion,
+  },
+  data: function () {
+    return {
+      lists: [
+        {
+          photo: require("../../../assets/image/board/sample01.jpg"),
+          nickname: "달콤팜",
+          title: "새콤달콤 설향딸기",
+          money: "4,200",
+        },
+        {
+          photo: require("../../../assets/image/board/sample01.jpg"),
+          nickname: "달콤팜",
+          title: "새콤달콤 설향딸기",
+          money: "4,200",
+        },
+      ],
+    };
+  },
 };
 </script>
